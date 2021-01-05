511174174c724dd73a7548c253e13c2703cd019c
ZacharyFoxLuke <72349944+ZacharyFoxLuke@users.noreply.github.com>
2021-01-04 22:50:21 -0500

    Initial "Worth It" files

---
+++ JavaScript/scripts_NEW.js
@@ -0,0 +1,90 @@
+// ---------------- The Brains of the Operation ---------------- //
+// ------------------------------------------------------------- //
+$modal = $('.modal-frame');
+$modal_Rewards_Info = $('#rewards-info-message');
+$modal_Transaction_Fee = $('#transactionFee-info-message'); 
+$modal_Profit = $('#profit-message');
+$modal_Loss = $('#loss-message');
+$modal_BreakEven = $('#break-even-message');
+$modal_Error = $('#error-message');
+$modal_About = $('#about-message');
+$modal_Contact = $('#contact-message');
+$dollarsProfit = $('.dollarsProfit');
+$dollarsFee = $('.dollarsFee');
+$dollarsNet = $('.dollarsNet'); 
+
+function transactionFeeInput() {
+    const fixedFee = document.getElementById('fixedFeeSection');
+    fixedFee.style.display = "inline-block";
+}
+
+function closeModal() {
+    $modal.removeClass('active');
+    $modal.addClass('leave');
+}
+
+$('.modal-overlay').click(function () {
+    closeModal();
+})
+
+$('.close').click(function () {
+    closeModal();
+})
+
+$('#moreInfo1').click(function () {
+    $modal_Rewards_Info.toggleClass('active');
+    $modal_Rewards_Info.removeClass('leave');
+})
+
+$('#moreInfo2').click(function () {
+    $modal_Transaction_Fee.toggleClass('active');
+    $modal_Transaction_Fee.removeClass('leave');
+})
+
+$('#about').click(function () {
+    $modal_About.toggleClass('active');
+    $modal_About.removeClass('leave');
+})
+
+$('#contact').click(function () {
+    $modal_Contact.toggleClass('active');
+    $modal_Contact.removeClass('leave');
+})
+
+$('#submitButton').click(function () {
+    const purchaseAmount = document.getElementById('purchaseAmountInput').value;
+    const rewardsRate = document.getElementById('rewardsRateInput').value / 100;
+    const transactionFeeFixed = document.getElementById('fixedFeeInput').value;
+    const earnedRewards = purchaseAmount * rewardsRate;
+    
+    if (purchaseAmount == '' || rewardsRate == '' || transactionFeeFixed == '') {
+        $modal_Error.toggleClass('active');
+        $modal_Error.removeClass('leave');
+
+    } else if (earnedRewards == transactionFeeFixed) {
+        $modal_BreakEven.toggleClass('active');
+        $modal_BreakEven.removeClass('leave');
+        $dollarsProfit.html(rewardsRate * purchaseAmount);
+        $dollarsFee.html(transactionFeeFixed);
+
+    } else if (earnedRewards < transactionFeeFixed) {
+        $modal_Loss.toggleClass('active');
+        $modal_Loss.removeClass('leave');
+        $dollarsProfit.html(rewardsRate * purchaseAmount);
+        $dollarsFee.html(transactionFeeFixed);
+        $dollarsNet.html(earnedRewards - transactionFeeFixed);
+
+    } else if (earnedRewards > transactionFeeFixed) {
+        $modal_Profit.toggleClass('active');
+        $modal_Profit.removeClass('leave');
+        $dollarsProfit.html(rewardsRate * purchaseAmount);
+        $dollarsFee.html(transactionFeeFixed);
+        $dollarsNet.html(earnedRewards - transactionFeeFixed);
+
+    }
+    
+});
+
+
+
+
